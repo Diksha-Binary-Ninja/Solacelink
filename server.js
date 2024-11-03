dccd6d69fbf5b4ead9fc1bf9b1d1508b5cdbaa87
Diksha-Binary-Ninja <139619268+Diksha-Binary-Ninja@users.noreply.github.com>
2024-11-03 14:18:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -230,7 +230,7 @@ io.on('connection', (socket) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
